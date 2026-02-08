81c1a9f65a0434ab38616d903102d8f3ee2a2dc3
uttam282005 <uttam282005@gmail.com>
2026-02-09 02:04:28 +0530

    feat: add workerServer struct

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/uttam282005/distrack
 
 go 1.25.4
+
+require (
+	github.com/google/uuid v1.6.0
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.10
+)
+
+require (
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+)
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,11 +1,48 @@
 // Package worker gets tasks to process
 package worker
 
-type WorkerServer struct {
+import (
+	"context"
+	"net"
+	"sync"
+	"time"
 
+	"github.com/google/uuid"
+	pb "github.com/uttam282005/distrack/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	DefaultHeartBeat int = 3
+)
+
+type WorkerServer struct {
+	pb.UnimplementedWorkerServiceServer
+
+	serverPort               string
+	coordinatorAddress       string
+	coordinatorServiceClient *pb.CoordinatorServiceClient
+	cooridnatorServiceConn   *grpc.ClientConn
+	heartbeatInterval        time.Duration
+	workerID                 uuid.UUID
+	taskQueue                chan *pb.TaskRequest
+	address                  string
+	wg                       sync.WaitGroup
+	ctx                      context.Context
+	cancel                   context.CancelFunc
+	listener                 net.Listener
+	grpcServer               grpc.Server
 }
 
-
-
-
-
+func NewServer(port string, coordinator string) *WorkerServer {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &WorkerServer{
+		workerID:           uuid.New(),
+		serverPort:         port,
+		coordinatorAddress: coordinator,
+		heartbeatInterval:  time.Duration(DefaultHeartBeat) * time.Second,
+		taskQueue:          make(chan *pb.TaskRequest, 100),
+		ctx:                ctx,
+		cancel:             cancel,
+	}
+}
