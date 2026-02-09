7f63f7363c1970df72c3d21ebf4c1a703059cac7
uttam282005 <uttam282005@gmail.com>
2026-02-09 23:10:18 +0530

    feat: worker

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -3,7 +3,11 @@ package worker
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net"
+	"os"
+	"os/exec"
 	"sync"
 	"time"
 
@@ -46,3 +50,55 @@ func NewServer(port string, coordinator string) *WorkerServer {
 		cancel:             cancel,
 	}
 }
+
+func(w *WorkerServer) setUpWorkerPool(workerCount int) {
+	for range workerCount {
+		w.wg.Add(1)
+		go w.worker()
+	}
+}
+
+func(w *WorkerServer) SubmitTask(ctx context.Context, task *pb.TaskRequest) (*pb.TaskResponse, error) {
+	log.Printf("Received task: %+v", task)
+
+	w.taskQueue <- task
+
+	return &pb.TaskResponse{
+		Message: "Task was submitted",
+		Success: true,
+		TaskId: task.TaskId,
+	}, nil
+}
+
+func(w *WorkerServer) worker() {
+	defer w.wg.Done()
+
+	for {
+		select {
+		case task := <-w.taskQueue:
+			go updateTaskStatus(pb.TaskStatus_INPROGRESS)
+			processTask(task)
+		  updateTaskStatus(pb.TaskStatus_COMPLETED)
+		case <-w.ctx.Done():
+			return
+		}
+	}
+}
+
+func(w *WorkerServer) processTask(task *pb.TaskRequest) {
+	commandString := task.Data
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
+
+	file, err := os.Create(fmt.Sprintf("%s_output.txt", w.workerID))
+	if (err != nil) {
+		log.Printf("worker: failed to create output file.")
+		return;
+	}
+	cmd := exec.CommandContext(ctx, commandString)
+	cmd.Stdout = file
+	cmd.Stderr = file
+
+	_, err = cmd.CombinedOutput()
+}
+
