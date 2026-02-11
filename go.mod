21966a5d0dd390db153c0630cd02d34cd20928f1
uttam282005 <uttam282005@gmail.com>
2026-02-11 21:57:09 +0530

    feat(schduler): add handle schedule tasks route handler

--- go.mod
+++ go.mod
@@ -11,16 +11,11 @@ require (
 )
 
 require (
-	github.com/cockroachdb/apd v1.1.0 // indirect
-	github.com/gofrs/uuid v4.4.0+incompatible // indirect
-	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/lib/pq v1.11.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/shopspring/decimal v1.4.0 // indirect
-	golang.org/x/crypto v0.44.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,7 +1,10 @@
+// Package scheduler insert tasks into database
 package scheduler
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,13 +12,34 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/jackc/pgx/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type SchduleTaskRequest struct {
+	Command     string `json:"command"`
+	ScheduledAt string `json:"scheduled_at"` // ISO 8601 format
+}
+
+type SchduleTaskResponse struct {
+	Success bool   `json:"success"`
+	TaskID  string `json:"taskId"`
+}
+
+type Task struct {
+	Id          string
+	Command     string
+	ScheduledAt pgtype.Timestamp
+	PickedAt    pgtype.Timestamp
+	StartedAt   pgtype.Timestamp
+	CompletedAt pgtype.Timestamp
+	FailedAt    pgtype.Timestamp
+}
+
 type SchedulerServer struct {
 	serverPort         string
-	httpServer             *http.Server
-	dbPool               *pgxpool.Pool
+	httpServer         *http.Server
+	dbPool             *pgxpool.Pool
 	dbConnectionString string
 	ctx                context.Context
 	cancel             context.CancelFunc
@@ -31,15 +55,99 @@ func NewServer(port string, dbConnectionString string) *SchedulerServer {
 	}
 }
 
-func (s *SchedulerServer) Start() {
+func (s *SchedulerServer) Start() error {
+	var err error
+	s.dbPool, err = s.connectToDatabase(s.ctx)
+	if err != nil {
+		log.Printf("database connection failed: %v", err)
+		return err
+	}
+
+	http.HandleFunc("/schedule", s.handleScheduleTask)
+	http.HandleFunc("/status", s.handleTaskStatus)
+
+	s.httpServer = &http.Server{
+		Addr: s.serverPort,
+	}
+
+	log.Printf("Starting scheduler server on %s\n", s.serverPort)
+
+	go func() {
+		if err := s.httpServer.ListenAndServe(); err != nil && er != http.ErrServerClosed {
+			log.Fatalf("Server error: %s\n", err)
+		}
+	}()
+
+	return s.awaitShutdown()
 }
 
-func ConnectToDatabase(ctx context.Context, dbConnectionString string) (*pgxpool.Pool, error) {
+func (s *SchedulerServer) handleScheduleTask(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Only POST request is allowerd", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var (
+		commandRequest  SchduleTaskRequest
+		commandResponse SchduleTaskResponse
+	)
+	if err := json.NewDecoder(r.Body).Decode(&commandRequest); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	log.Printf("Received schedule request: %+v", commandRequest)
+
+	scheduledTime, err := time.Parse(time.RFC3339, commandRequest.ScheduledAt)
+	if err != nil {
+		http.Error(w, "Invalid date format. Use ISO 8601 format.", http.StatusBadRequest)
+		return
+	}
+
+	unixTimestamp := time.Unix(scheduledTime.Unix(), 0)
+
+	task := Task{
+		Command:     commandRequest.Command,
+		ScheduledAt: pgtype.Timestamp{Time: unixTimestamp},
+	}
+	taskID, err := s.insertIntoDB(context.Background(), task)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to submit task. Error: %s", err.Error()),
+			http.StatusInternalServerError)
+		return
+	}
+
+	commandResponse.TaskID = taskID
+	commandResponse.Success = true
+
+	jsonResponse, err := json.Marshal(commandResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write(jsonResponse)
+}
+
+func (s *SchedulerServer) handleTaskStatus() {
+}
+
+func (s *SchedulerServer) insertIntoDB(ctx context.Context, task Task) (string, error) {
+	sqlStatement := "INSERT INTO tasks (command, scheduled_at) VALUES ($1, $2) RETURNING id"
+
+	var taskID string
+	err := s.dbPool.QueryRow(ctx, sqlStatement, task.Command, task.ScheduledAt).Scan(&taskID)
+	if err != nil {
+		return "", err
+	}
+
+	return taskID, nil
+}
+
+func (s *SchedulerServer) connectToDatabase(ctx context.Context) (*pgxpool.Pool, error) {
 	var dbPool *pgxpool.Pool
 	var err error
 	retryCount := 0
 	for retryCount < 5 {
-		dbPool, err = pgxpool.New(ctx, dbConnectionString)
+		dbPool, err = pgxpool.New(ctx, s.dbConnectionString)
 		if err == nil {
 			break
 		}
