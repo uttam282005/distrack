096e96c86666026ebee83dc9280ce8d85efb0eb0
uttam282005 <uttam282005@gmail.com>
2026-02-12 22:36:15 +0530

    coordinator: add skeleton

--- internal/coordinator/coordinator.go
+++ internal/coordinator/coordinator.go
@@ -5,24 +5,36 @@ import (
 	"context"
 	"net"
 	"sync"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	pb "github.com/uttam282005/distrack/proto"
 	"google.golang.org/grpc"
 )
 
-const (
-	allowedHeartbearMisses = 3
-)
-
-type CoordinatorServerService struct {
+type CoordinatorServer struct {
 	pb.UnimplementedCoordinatorServiceServer
 
-	serverPort string
-	dbPool     *pgxpool.Pool
-	server     *grpc.Server
-	listener   net.Listener
-	ctx        context.Context
-	cancel     context.CancelFunc
-	wg         sync.WaitGroup
+	serverPort          string
+	listener            net.Listener
+	grpcServer          *grpc.Server
+	WorkerPool          map[uint32]*WorkerInfo
+	WorkerPoolMutex     sync.Mutex
+	WorkerPoolKeys      []uint32
+	WorkerPoolKeysMutex sync.RWMutex
+	maxHeartbeatMisses  uint8
+	heartbeatInterval   time.Duration
+	roundRobinIndex     uint32
+	dbConnectionString  string
+	dbPool              *pgxpool.Pool
+	ctx                 context.Context
+	cancel              context.CancelFunc
+	wg                  sync.WaitGroup
+}
+
+type WorkerInfo struct {
+	heartbeatMisses     uint8
+	conn                *grpc.ClientConn
+	address             string
+	workerServiceClient *pb.WorkerServiceClient
 }
